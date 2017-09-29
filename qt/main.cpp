c2b41c15c32edeae9cf8bccbf3aad7fa588df94c
Ben Shealy <bentshermann@gmail.com>
2017-09-29 14:06:11 -0400

    Worked on main.cpp

--- qt/main.cpp
+++ qt/main.cpp
@@ -1,20 +1,12 @@
-#include <getopt.h>
 #include <QApplication>
 #include <QSurfaceFormat>
 #include "database.h"
 #include "mainwindow.h"
 
-typedef enum {
-    OPTION_ONTFILE,
-    OPTION_JSONFILE,
-    OPTION_HELP,
-    OPTION_UNKNOWN = '?'
-} option_t;
-
 typedef struct {
-    const char *ont_file;
-    const char *json_file;
-} optarg_t;
+    QString config;
+    QString ont;
+} args_t;
 
 /**
  * Print command-line usage.
@@ -25,70 +17,58 @@ void print_usage()
         "Usage: ./biodep-vis [options]\n"
         "\n"
         "Options:\n"
-        "  --ont_file   FILE    [REQUIRED] specify the ontology text file\n"
-        "  --json_file  FILE    [REQUIRED] specify the json file containing graphs and alignments\n"
-        "  --help       list help options\n"
+        "  --config FILE  configuration file [input.json]\n"
+        "  --ont FILE     ontology dictionary file [go-basic.obo]\n"
+        "  --help         list help options\n"
     );
 }
 
 int main(int argc, char *argv[])
 {
+    QApplication app(argc, argv);
+
     // parse command-line arguments
-    optarg_t args = {
-        "go-basic.obo",
-        "input.json"
+    args_t args = {
+        "input.json",
+        "go-basic.obo"
     };
 
-    struct option long_options[] = {
-        { "ont_file", required_argument, 0, OPTION_ONTFILE },
-        { "json_file", required_argument, 0, OPTION_JSONFILE },
-        { "help", no_argument, 0, OPTION_HELP },
-        { 0, 0, 0, 0 }
-    };
+    QStringList options = app.arguments();
 
-    int opt;
-    while ( (opt = getopt_long_only(argc, argv, "", long_options, NULL)) != -1 ) {
-        switch ( opt ) {
-        case OPTION_ONTFILE:
-            args.ont_file = optarg;
-            break;
-        case OPTION_JSONFILE:
-            args.json_file = optarg;
-            break;
-        case OPTION_HELP:
+    for ( int i = 1; i < options.size(); i++ ) {
+        if ( options[i] == "--config" ) {
+            args.config = options[i + 1];
+            i++;
+        }
+        else if ( options[i] == "--ont" ) {
+            args.ont = options[i + 1];
+            i++;
+        }
+        else if ( options[i] == "--help" ) {
             print_usage();
-            exit(1);
-        case OPTION_UNKNOWN:
+            exit(0);
+        }
+        else {
             print_usage();
             exit(1);
         }
     }
 
-    // validate arguments
-    if ( !args.ont_file || !args.json_file ) {
-        print_usage();
-        exit(1);
-    }
-
-    // load data
-    Database db;
-
-    db.load_config(args.json_file);
-    db.load_ontology(args.ont_file);
-
-    // start application
-    QApplication a(argc, argv);
-
+    // initialize OpenGL format
     QSurfaceFormat format;
     format.setDepthBufferSize(24);
-    format.setStencilBufferSize(8);
     format.setVersion(3, 2);
     format.setProfile(QSurfaceFormat::CoreProfile);
     QSurfaceFormat::setDefaultFormat(format);
 
-    MainWindow w(&db);
-    w.resize(1280, 720);
-    w.show();
+    // load data
+    Database db;
+    db.load_config(args.config);
+    db.load_ontology(args.ont);
 
-    return a.exec();
+    // initialize window
+    MainWindow window(&db);
+    window.showMaximized();
+
+    return app.exec();
 }
