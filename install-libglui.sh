f8e7c7708fd16572c16153b980e40d6252111e02
Ben Shealy <bentshermann@gmail.com>
2017-10-19 11:23:16 -0400

    Moved install-libglui code into Makefile-old

--- install-libglui.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Script to install libglui.
-
-wget https://github.com/libglui/glui/archive/2.37.zip -O glui-2.37.zip
-unzip glui-2.37.zip
-
-cd glui-2.37 && make && cd ..
-
-rm -rf glui-2.37.zip
