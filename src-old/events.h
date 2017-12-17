759f5502e9e0de006acafed9bc9193dc8028e018
Ben Shealy <bentshermann@gmail.com>
2017-12-17 15:36:38 -0500

    Removed old files

--- src-old/events.h
+++
@@ -1,9 +0,0 @@
-#ifndef _EVENTS_H_
-#define _EVENTS_H_
-
-void mouseEventHandler(int button, int state, int x, int y);
-void motionEventHandler(int x, int y);
-void keyboardEventHandler(unsigned char key, int x, int y);
-void control_cb(int control);
-
-#endif
